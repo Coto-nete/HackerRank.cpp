8b49a8faf3859f6e851067732d33b8eb7e5f56fc
Felipe Nunes <felipe_nunes@discente.ufg.br>
2024-04-10 23:35:20 -0300

    doing

---
+++ exercices/Botas Trocadas.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int nn,tempi;
+int size[10000];
+string foot[10000];
+string temps,temp;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> nn;
+    for(int i=0;i<=nn;i++){
+        
+    }
+
+    return 0;
+}
